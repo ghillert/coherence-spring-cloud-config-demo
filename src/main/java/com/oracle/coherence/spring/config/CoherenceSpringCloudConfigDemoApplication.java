f681d8f52a5a4cc090530cc268bcbc7e2ee99374
Gunnar Hillert <gunnar.hillert@oracle.com>
2021-01-30 23:15:48 -1000

    Initial commit

---
+++ src/main/java/com/oracle/coherence/spring/config/CoherenceSpringCloudConfigDemoApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.coherence.spring.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoherenceSpringCloudConfigDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoherenceSpringCloudConfigDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oracle/coherence/spring/config/CoherenceSpringCloudConfigDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.coherence.spring.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoherenceSpringCloudConfigDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
